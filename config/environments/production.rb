212a20405cbfd35749be5633ec785495404c35b2
Oren Teich <oren@heroku.com>
2010-05-17 18:31:05 -0700

    cache-money & bundler

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,20 +9,15 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
-# See everything in the log (default is :info)
-# config.log_level = :debug
+config.cache_store = :mem_cache_store, Memcached::Rails.new
 
-# Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
+require 'cache_money'
+$memcache = Memcached.new
 
-# Use a different cache store in production
-# config.cache_store = :mem_cache_store
+$local = Cash::Local.new($memcache)
+$lock = Cash::Lock.new($memcache)
+$cache = Cash::Transactional.new($local, $lock)
 
-# Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host = "http://assets.example.com"
-
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
-
-# Enable threaded mode
-# config.threadsafe!
+class ActiveRecord::Base
+  is_cached :repository => $cache
+end
